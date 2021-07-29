a6fc9f0c6bef47981edde06e96507b64014b339f
tobias <tobias.leong@zendesk.com>
2021-07-30 00:03:53 +0800

    Finish exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,42 +1,58 @@
-import React from 'react'
+import React from "react";
 
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+  return <h1>{props.course}</h1>;
+};
 
 const Content = (props) => {
+  // using a loop to iterate through all the
+  // props given in arrays (parts, exercises)
+  const allParts = [];
+
+  props.parts.forEach((x, i) => {
+    let element = <Part part={x} exercise={props.exercises[i]} />;
+    allParts.push(element);
+  });
+
+  return <div>{allParts}</div>;
+};
+
+const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
 
 const Total = (props) => {
   const total = props.exercise1 + props.exercise2 + props.exercise3;
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  const part2 = 'Using props to pass data';
+  const part2 = "Using props to pass data";
   const exercises2 = 7;
-  const part3 = 'State of a component';
+  const part3 = "State of a component";
   const exercises3 = 14;
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total
+        exercise1={exercises1}
+        exercise2={exercises2}
+        exercise3={exercises3}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
