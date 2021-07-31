a0a649d8bae965d9728cda333e32f23b012ad61f
tobias <tobias.leong@zendesk.com>
2021-07-31 11:23:42 +0800

    Refactor

--- part1/partb/src/App.js
+++ part1/partb/src/App.js
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age + myState.ageDiff;
-
-  const initialState = {
-    ageDiff: 0,
-  };
-  const [myState, setMyState] = useState(initialState);
-
-  // increases age
-  setTimeout(
-    () => setMyState({ ...myState, ageDiff: myState.ageDiff + 1 }),
-    2000
-  );
+const Hello = ({ name, age, counter }) => {
+  const bornYear = () => new Date().getFullYear() - age + counter;
 
   return (
     <div>
       <p>
-        Hello {name}, you are {age + myState.ageDiff} years old. ageDiff:{" "}
-        {myState.ageDiff}
+        Hello {name}, you are {age + counter} years old. ageDiff:{counter}
       </p>
       <p>
         <b>So you were probably born in {bornYear()}</b>
@@ -38,14 +26,14 @@ const App = () => {
   const [myState, setMyState] = useState(initialState);
 
   const handleMouseEnter = () => {
-    console.log("Mouse enter detected...")
-    setMyState({...myState, greeting: "MOUSEOVER"})
-  }
+    console.log("Mouse enter detected...");
+    setMyState({ ...myState, greeting: "MOUSEOVER" });
+  };
 
   const handleMouseLeave = () => {
-    console.log("Mouse leave detected...")
-    setMyState({...myState, greeting: "Greetings"})
-  }
+    console.log("Mouse leave detected...");
+    setMyState({ ...myState, greeting: "Greetings" });
+  };
 
   console.log("rendering...", myState.counter);
   return (
@@ -66,10 +54,11 @@ const App = () => {
           })
         }
       >
-        Click!
+        Increase
       </button>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <button onClick={() => setMyState(initialState)}>Reset</button>
+      <Hello name="Maya" age={26 + 10} counter={myState.counter} />
+      <Hello name={name} age={age} counter={myState.counter} />
     </div>
   );
 };
