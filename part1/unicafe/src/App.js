24066c093889e39d59d7a717b9bf496d91be73c1
tobias <tobias.leong@zendesk.com>
2021-08-01 09:16:08 +0800

    Add ex 1.7 and 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const Statistics = ({good, neutral, bad}) => {
-  return(
+const Statistics = ({ good, neutral, bad, stats }) => {
+  return (
     <div>
       <h1>statistics</h1>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {stats.total}</p>
+      <p>average: {stats.average}</p>
+      <p>positive: {stats.positivePercentage}%</p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const initialStats = {
+    all: 0,
+    average: 0,
+    positivePercentage: 0,
+  };
+
+  const [stats, setStats] = useState(initialStats);
+
+  // lift state up to app component
+  useEffect(() => {
+    let total = good + neutral + bad;
+    let average = (good - bad) / total;
+    let positivePercentage = good / total;
+
+    setStats({
+      total: total,
+      average: average,
+      positivePercentage: positivePercentage,
+    });
+  }, [good, neutral, bad]);
 
   return (
     <div>
@@ -23,9 +46,9 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} stats={stats} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
