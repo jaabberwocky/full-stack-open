8de7200caeaca73718743456a7b6a898e74242eb
tobias <tobias.leong@zendesk.com>
2021-08-02 21:46:42 +0800

    Complete ex 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,28 +9,34 @@ const Entry = ({ person }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" , id: 0}]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
   const addNewName = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
-      id: persons.length
     };
-    setPersons(persons.concat(nameObject));
+
+    for (let el of persons) {
+      if (nameObject.name === el.name) {
+        alert(`${nameObject.name} is already added to phonebook`);
+      } else {
+        setPersons(persons.concat(nameObject));
+      }
+    }
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -38,7 +44,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <Entry key={person.id} person={person} />
+        <Entry key={person.name} person={person} />
       ))}
     </div>
   );
