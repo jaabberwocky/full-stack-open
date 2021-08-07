5f7afb195f977c0ce76033f9bbffbf97302d0bfb
tobias <tobias.leong@zendesk.com>
2021-08-08 00:17:20 +0800

    Add custom middleware

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -24,7 +24,21 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+// middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(express.json());
+app.use(requestLogger);
+app.use(unknownEndpoint);
 
 // helper functions
 const generateId = () => {
