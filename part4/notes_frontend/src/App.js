ec09e3a7142f0b479f75f81720afbb1a2ca5f4b6
tobias <tobias.leong@zendesk.com>
2021-08-17 22:20:23 +0800

    Add part4

---
+++ part4/notes_frontend/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    noteService.getAll().then((notes) => {
+      setNotes(notes);
+    });
+  };
+  useEffect(hook, [showAll]); // if the second arg is empty array, only run on first render
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5, // 50% chance to be important
+    };
+    noteService.create(noteObject).then((resp) => {
+      setNotes(notes.concat(resp));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    noteService.update(id, changedNote).then((resp) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : resp)));
+    });
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <br />
+      <i>Showing {showAll ? "all" : "important"} notes</i>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ part4/notes_frontend/src/services/notes.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const baseUrl = "/api/notes";
+
+const getAll = () => {
+  const req = axios.get(baseUrl);
+  return req.then((resp) => resp.data);
+};
+
+const create = (newObject) => {
+  const req = axios.post(baseUrl, newObject);
+  return req.then((resp) => resp.data);
+};
+
+const update = (id, newObject) => {
+  const req = axios.put(`${baseUrl}/${id}`, newObject);
+  return req.then((resp) => resp.data);
+};
+
+const obj = {
+  getAll,
+  create,
+  update,
+};
+export default obj;
