124bc93c3e85919270a70fa7f067e94818718c07
tobias <tobias.leong@zendesk.com>
2021-08-19 00:43:57 +0800

    Add ex 4.5-4.7

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -65,3 +65,215 @@ describe("totalLikes", () => {
     expect(result).toBe(30);
   });
 });
+
+describe("favouriteBlog", () => {
+  test("empty returns empty object", () => {
+    const blogs = [];
+
+    const result = listHelper.favouriteBlog(blogs);
+    expect(result).toEqual({});
+  });
+
+  test("single blog returns single blog", () => {
+    const blogs = [
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.favouriteBlog(blogs);
+    expect(result).toEqual({
+      title: "Go To Statement Considered Harmful 3",
+      author: "Edsger W. Dijkstra",
+      likes: 10,
+    });
+  });
+  test("return correct blog with multiple blogs", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "asddasd123123",
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+
+    const favBlog = {
+      title: "Go To Statement Considered Harmful 2",
+      author: "Edsger W. Dijkstra",
+      likes: 15,
+    };
+
+    const result = listHelper.favouriteBlog(blogs);
+    expect(result).toEqual(favBlog);
+  });
+});
+
+describe("mostBlogs", () => {
+  test("correct author is returned with multiple blogs", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "asddasd123123",
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({
+      author: "Edsger W. Dijkstra",
+      blogs: 3,
+    });
+  });
+
+  test("empty obj is returned with zero blogs", () => {
+    const blogs = [];
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({});
+  });
+
+  test("right obj is returned with multiple blogs with different authors", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "ASDF",
+        author: "James Arthur",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "asddasd123123",
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({ author: "Edsger W. Dijkstra", blogs: 2 });
+  });
+});
+
+describe("mostLikes", () => {
+  test("correct author and likes is returned with multiple blogs", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "asddasd123123",
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.mostLikes(blogs);
+    expect(result).toEqual({
+      author: "Edsger W. Dijkstra",
+      likes: 30,
+    });
+  });
+
+  test("empty obj is returned with zero blogs", () => {
+    const blogs = [];
+    const result = listHelper.mostLikes(blogs);
+    expect(result).toEqual({});
+  });
+
+  test("right obj is returned with multiple blogs with different authors", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "ASDF",
+        author: "James Arthur",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "asddasd123123",
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+      {
+        _id: "91209dlasd9adsl;",
+        title: "Go To Statement Considered Harmful 3",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.mostLikes(blogs);
+    expect(result).toEqual({ author: "Edsger W. Dijkstra", likes: 25 });
+  });
+});
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,14 +1,82 @@
+const _ = require("lodash");
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
 const totalLikes = (blogs) => {
-    // have to pass initial value as it is an array of objects
-    return blogs.reduce((accum, currentBlog) => {return accum + currentBlog.likes}, 0)
-}
+  // have to pass initial value as it is an array of objects
+  return blogs.reduce((accum, currentBlog) => {
+    return accum + currentBlog.likes;
+  }, 0);
+};
+
+const favouriteBlog = (blogs) => {
+  let highestBlog = {};
+
+  for (let blog of blogs) {
+    if (blog.likes > highestBlog.likes || highestBlog.likes === undefined) {
+      highestBlog = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes,
+      };
+    }
+  }
+  return highestBlog;
+};
+
+const mostBlogs = (blogs) => {
+  const authorCount = {};
+
+  for (let blog of blogs) {
+    if (blog.author in authorCount) {
+      authorCount[blog.author] += 1;
+    } else {
+      authorCount[blog.author] = 1;
+    }
+  }
+
+  const maxAuthor = _.maxBy(_.keys(authorCount), function (o) {
+    return authorCount[o];
+  });
+  const blogCount = authorCount[maxAuthor];
+  return maxAuthor === undefined || blogCount === undefined
+    ? {}
+    : {
+        author: maxAuthor,
+        blogs: blogCount,
+      };
+};
+
+const mostLikes = (blogs) => {
+  const authorCount = {};
+
+  for (let blog of blogs) {
+    if (blog.author in authorCount) {
+      authorCount[blog.author] += blog.likes;
+    } else {
+      authorCount[blog.author] = blog.likes;
+    }
+  }
+
+  const maxAuthor = _.maxBy(_.keys(authorCount), function (o) {
+    return authorCount[o];
+  });
+  const likes = authorCount[maxAuthor];
+  return maxAuthor === undefined || likes === undefined
+    ? {}
+    : {
+        author: maxAuthor,
+        likes: likes,
+      };
+};
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes,
 };
