da9b302bce85b394f4edfb888b71581b1d1a2d23
Tobias Leong <tobias.leong@zendesk.com>
2021-10-12 00:11:28 +0800

    Add Ex 5.7

--- part5/exercises/bloglist-frontend/src/components/Blog.js
+++ part5/exercises/bloglist-frontend/src/components/Blog.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 
-const Blog = ({ blog }) => (
-    <Card style={{ width: '18rem' }}>
-        <Card.Body>
-            <Card.Title>{blog.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-                {blog.author}
-            </Card.Subtitle>
-            <Card.Text>URL: {blog.url}</Card.Text>
-        </Card.Body>
-    </Card>
-);
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false);
+
+    const showWhenVisible = { display: visible ? '' : 'none' };
+    const showWhenNotVisible = { display: visible ? 'none' : '' };
+
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{blog.title}</Card.Title>
+                <div style={showWhenNotVisible}>
+                    <button onClick={() => setVisible(true)}>view</button>
+                </div>
+                <div style={showWhenVisible}>
+                    <button onClick={() => setVisible(false)}>hide</button>
+                    <Card.Subtitle className="mb-2 text-muted">
+                        {blog.author}
+                    </Card.Subtitle>
+                    <Card.Text>URL: {blog.url}</Card.Text>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+};
 
 export default Blog;
